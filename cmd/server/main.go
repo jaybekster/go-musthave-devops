878db101c97c1c08383050404c607bc3a3c86f9a
Mikhail Tsyplakov <jaybekster@gmail.com>
2021-09-05 02:41:52 +0300

    server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,29 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		m, _ := url.ParseQuery(r.URL.RawQuery)
+
+		metricId := m["id"]
+		metricType := m["type"]
+		metricValue := m["value"]
+
+		fmt.Println(metricId, metricType, metricValue)
+	default:
+		fmt.Fprintf(w, "Sorry, only POST methods are supported.")
+	}
+}
+
+func main() {
+	http.HandleFunc("/", mainHandler)
+
+	log.Fatal(http.ListenAndServe(":8880", nil))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jaybekster/go-musthave-devops
+
+go 1.16
